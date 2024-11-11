0f49a023dead6c3afa49660b132583ccda18a7ae
Aviendha <aviendha.aviendha@gmail.com>
2024-11-11 01:08:18 -0800

    launch with talker

---
+++ launch/pure_pursuit_launch.py
@@ -0,0 +1,20 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch_ros.actions import Node
+from launch.substitutions import LaunchConfiguration, TextSubstitution, PathJoinSubstitution
+
+def generate_launch_description():
+	pursuit_node = Node(
+		package='pure_pursuit',
+		executable='pure_pursuit_node.py',
+		name='pure_pursuit',
+		output='screen',
+		# parameters=[{'speed':LaunchConfiguration('speed', default='0.0'),
+		# 	'max_turn_angle':LaunchConfiguration('max_turn_angle', default='0.0'),
+		# 	'look_ahead':LaunchConfiguration('look_ahead', default='0.0')}
+		# ]
+	)
+
+	return LaunchDescription([
+        pursuit_node
+	])
--- scripts/pure_pursuit_node.py
+++ scripts/pure_pursuit_node.py
@@ -2,39 +2,80 @@
 import rclpy
 from rclpy.node import Node
 
-import numpy as np
-from sensor_msgs.msg import LaserScan
-from ackermann_msgs.msg import AckermannDriveStamped, AckermannDrive
-# TODO CHECK: include needed ROS msg type headers and libraries
+from std_msgs.msg import String
 
-class PurePursuit(Node):
-    """ 
-    Implement Pure Pursuit on the car
-    This is just a template, you are free to implement your own node!
-    """
+
+class MinimalPublisher(Node):
+
     def __init__(self):
-        super().__init__('pure_pursuit_node')
-        # TODO: create ROS subscribers and publishers
+        super().__init__('minimal_publisher')
+        self.publisher_ = self.create_publisher(String, 'topic', 10)
+        timer_period = 0.5  # seconds
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.i = 0
 
-    def pose_callback(self, pose_msg):
-        pass
-        # TODO: find the current waypoint to track using methods mentioned in lecture
+    def timer_callback(self):
+        msg = String()
+        msg.data = 'Hello World: %d' % self.i
+        self.publisher_.publish(msg)
+        self.get_logger().info('Publishing: "%s"' % msg.data)
+        self.i += 1
 
-        # TODO: transform goal point to vehicle frame of reference
-
-        # TODO: calculate curvature/steering angle
-
-        # TODO: publish drive message, don't forget to limit the steering angle.
 
 def main(args=None):
     rclpy.init(args=args)
-    print("PurePursuit Initialized")
-    pure_pursuit_node = PurePursuit()
-    rclpy.spin(pure_pursuit_node)
 
-    pure_pursuit_node.destroy_node()
+    minimal_publisher = MinimalPublisher()
+
+    rclpy.spin(minimal_publisher)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    minimal_publisher.destroy_node()
     rclpy.shutdown()
 
 
 if __name__ == '__main__':
     main()
+
+# #!/usr/bin/env python3
+# import rclpy
+# from rclpy.node import Node
+
+# import numpy as np
+# from sensor_msgs.msg import LaserScan
+# from ackermann_msgs.msg import AckermannDriveStamped, AckermannDrive
+# # TODO CHECK: include needed ROS msg type headers and libraries
+
+# class PurePursuit(Node):
+#     """ 
+#     Implement Pure Pursuit on the car
+#     This is just a template, you are free to implement your own node!
+#     """
+#     def __init__(self):
+#         super().__init__('pure_pursuit_node')
+#         # TODO: create ROS subscribers and publishers
+
+#     def pose_callback(self, pose_msg):
+#         pass
+#         # TODO: find the current waypoint to track using methods mentioned in lecture
+
+#         # TODO: transform goal point to vehicle frame of reference
+
+#         # TODO: calculate curvature/steering angle
+
+#         # TODO: publish drive message, don't forget to limit the steering angle.
+
+# def main(args=None):
+#     rclpy.init(args=args)
+#     print("PurePursuit Initialized")
+#     pure_pursuit_node = PurePursuit()
+#     rclpy.spin(pure_pursuit_node)
+
+#     pure_pursuit_node.destroy_node()
+#     rclpy.shutdown()
+
+
+# if __name__ == '__main__':
+#     main()
